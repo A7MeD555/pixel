688c35af26459eb60235f1f0e575a112e29bc71d
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-10-24 00:19:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -804,7 +804,7 @@ client.on('message', function(message) {
             .setThumbnail(`${message.author.avatarURL}`)
             .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
             .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
-        client.channels.get("503609022625349633").send({ embed: stewart });
+        client.channels.get("483760502254600193").send({ embed: stewart });
     }
 });
     
